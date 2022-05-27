80debef1046e1e504954dedc52147c25160d9471
lufeijun1234 <lufeijun_1234@126.com>
2022-05-27 13:45:09 +0800

    first commit

---
+++ src/HelloComposer.php
@@ -0,0 +1,14 @@
+<?php
+namespace Lufeijun1234;
+
+/**
+ * 熟悉 composer 包流程
+ */
+class HelloComposer
+{
+  
+  public static function sayhello()
+  {
+      echo "Hello2, Composer!", PHP_EOL;
+  }
+}
